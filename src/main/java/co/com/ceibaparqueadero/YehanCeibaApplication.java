1f666edbaa03053b94c20f1e8c3bb8d491af4183
yehanportilla <jancha22@gmail.com>
2019-10-20 21:31:10 -0500

    Conexion a base de datos Mysql5 bd_parqueadero con la configuracion en application.properties funciona

--- src/main/java/co/com/ceibaparqueadero/YehanCeibaApplication.java
+++ src/main/java/co/com/ceibaparqueadero/YehanCeibaApplication.java
@@ -2,8 +2,10 @@ package co.com.ceibaparqueadero;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"co.com.ceibaparqueadero.infraestructura.persistencia.entidades"})
 public class YehanCeibaApplication {
 
 	public static void main(String[] args) {
